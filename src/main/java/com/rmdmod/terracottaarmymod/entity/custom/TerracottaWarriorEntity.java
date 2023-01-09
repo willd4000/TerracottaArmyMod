74c737ec4e9b064bd97057cc095b227c359d528d
willd <willd4000@gmail.com>
2023-01-08 18:41:20 -0600

    currently planning to add mob behavior for TerracottaWarriorEntity. This entity inherits from TamableAnimal.

--- src/main/java/com/rmdmod/terracottaarmymod/entity/custom/TerracottaWarriorEntity.java
+++ src/main/java/com/rmdmod/terracottaarmymod/entity/custom/TerracottaWarriorEntity.java
@@ -1,3 +1,50 @@
 package com.rmdmod.terracottaarmymod.entity.custom;
-public class TerracottaWarriorEntity{
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+
+
+public class TerracottaWarriorEntity extends TamableAnimal implements GeoEntity {
+    private AnimatableInstanceCache cache = new AnimatableInstanceCache(this) {
+        @Override
+        public <T extends GeoAnimatable> AnimatableManager<T> getManagerForId(long uniqueId) {
+            return null;
+        }
+    };
+    public TerracottaWarriorEntity(EntityType<? extends TamableAnimal> p_21803_, Level p_21804_) {
+        super(p_21803_, p_21804_);
+    }
+
+    public static AttributeSupplier setAttributes(){
+        return Animal.createMobAttributes().add(Attributes.MAX_HEALTH, 40)
+                .add(Attributes.ATTACK_DAMAGE, 8f)
+                .add(Attributes.ATTACK_SPEED, 2f)
+                .add(Attributes.MOVEMENT_SPEED, .3f).build();
+    }
+    @Nullable
+    @Override
+    public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
+        return null;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return null;
+    }
 }
